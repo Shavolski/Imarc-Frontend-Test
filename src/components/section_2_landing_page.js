1f1808bd20f1a7073ab3a420c64852ecb37f478c
Shavolski-dev <stevewachiradev@gmail.com>
2020-11-11 17:05:40 -0500

    completed the landing page

--- src/components/section_2_landing_page.js
+++ src/components/section_2_landing_page.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-//Landing_page section
+//Landing page section
 function Landing_page() {
   return (
-    <div className="landing-page">
-      <h1>Landing page</h1>
+
+    {/* Image first and it's wrapped in a div for the text */}
+    <div className="landing-page w-screen">
+
+      {/* space to and from the picture */}
+      <div className="py-48 pl-48">
+        <h1 className="font-bold text-white text-6xl pb-20">LOREM IPSUM</h1>
+        {/* The smaller text */}
+        <div className="flex flex-col">
+          <p className="text-lg">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+      </div>
     </div>
   );
 };
