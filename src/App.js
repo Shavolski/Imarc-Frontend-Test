1e30359fad7ae3c05fb7b609f1a496a077a45032
Shavolski-dev <stevewachiradev@gmail.com>
2020-11-11 16:15:30 -0500

    imported landing page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './styles/App.scss';
 import Navbar from './components/section_1_navbar.js';
+import Landing_page from './components/section_2_landing_page.js';
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <Landing_page/>
     </div>
   );
 }
