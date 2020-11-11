501b4eae6c31864525236d54372fa43f8fee13bb
Shavolski-dev <stevewachiradev@gmail.com>
2020-11-11 14:45:44 -0500

    navbar setup and imported colors

--- src/components/section_1_navbar.js
+++ src/components/section_1_navbar.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import logo from '../logo.svg';
 
 //Navbar section
 function Navbar() {
   return (
-    <div>
-      <h1>Navbar</h1>
+    <div className="pt-4">
+      <img src={logo} />
+      <div>
+        <a href="#" className="mt-1 block px-2 py-1 text-white font-semibold hover:bg-gray-800">List your property</a>
+        <a href="#" className="mt-1 block px-2 py-1 text-white font-semibold hover:bg-gray-800">Trips</a>
+        <a href="#" className="mt-1 block px-2 py-1 text-white font-semibold hover:bg-gray-800">Messages</a>
+      </div>
     </div>
   );
 };
+
+export default Navbar;
