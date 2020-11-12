c3f2cb2b677e249f7764a59b460fb6bafa50c774
Shavolski-dev <stevewachiradev@gmail.com>
2020-11-11 19:45:28 -0500

    created a grid mockup

--- src/components/section_4_grid.js
+++ src/components/section_4_grid.js
@@ -4,8 +4,19 @@ import ReactDOM from 'react-dom';
 //Grid section
 function Grid() {
   return (
-    <div>
-      <h1>Grid</h1>
+    <div class="flex bg-gray-200 mb-10 mx-48">
+      <div class="w-1/3 bg-gray-200">fff</div>
+
+      <div class="w-1/3 bg-gray-200">
+        <div class="text-gray-700 text-center bg-gray-400 px-4 py-1 m-1">1</div>
+        <div class="text-gray-700 text-center bg-gray-400 px-4 py-1 m-1">2</div>
+      </div>
+
+      <div class="w-1/3 bg-gray-200">
+        <div class="text-gray-700 text-center bg-gray-400 px-4 py-1 m-1">1</div>
+        <div class="text-gray-700 text-center bg-gray-400 px-4 py-1 m-1">2</div>
+        <div class="text-gray-700 text-center bg-gray-400 px-4 py-1 m-1">3</div>
+      </div>
     </div>
   );
 };
