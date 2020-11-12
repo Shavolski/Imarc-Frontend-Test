08ab10f44fdd30a3ec990b02e775d229a8ad2f0a
Shavolski-dev <stevewachiradev@gmail.com>
2020-11-12 07:31:22 -0500

    build error in netlify becuase the landing page was being identified as a jsx component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './styles/App.scss';
 import Navbar from './components/section_1_navbar.js';
-import Landing_page from './components/section_2_landing_page.js';
+import LandingPage from './components/section_2_landing_page.js';
 import Mini from './components/section_3_mini_links.js';
 import Grid from './components/section_4_grid.js';
 import Learn from './components/section_5_learn.js';
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="font-body static">
       <Navbar className="absolute"/>
-      <Landing_page/>
+      <LandingPage/>
       <Mini/>
       <Grid/>
       <Learn/>
