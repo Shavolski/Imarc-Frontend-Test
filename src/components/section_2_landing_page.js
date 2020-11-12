3e46c7777c79491e9a7e023dd6e7de63aabf2b47
Shavolski-dev <stevewachiradev@gmail.com>
2020-11-11 19:24:10 -0500

    completed the mini section

--- src/components/section_2_landing_page.js
+++ src/components/section_2_landing_page.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 //Landing page section
 function Landing_page() {
   return (
-    <div className="landing-page w-screen h-full shadow-2xl -mt-6">
+    <div className="landing-page w-screen h-full -mt-6">
       {/* space to and from the picture */}
       <div className="py-48 pl-48">
         <h1 className="font-bold text-white text-6xl pb-20">LOREM IPSUM</h1>
