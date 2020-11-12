0509511be938368284dfeae0c0a0bcfc443aa6be
Shavolski-dev <stevewachiradev@gmail.com>
2020-11-11 20:28:40 -0500

    imported the background images into the grids and they fit the div

--- src/components/section_4_grid.js
+++ src/components/section_4_grid.js
@@ -5,16 +5,16 @@ import ReactDOM from 'react-dom';
 function Grid() {
   return (
     <div class="flex bg-gray-200 mb-10 mx-48">
-      <div class="w-1/3 bg-gray-400 m-1 py-64">full space</div>
+      <div class="grid_1 w-1/3 bg-gray-400 m-1 py-64">full space</div>
 
       <div class="w-1/3 bg-gray-200">
-        <div class="h-1/2 text-gray-700 text-center bg-gray-400 px-2 m-1 py-32">1</div>
-        <div class="h-1/2 text-gray-700 text-center bg-gray-400 px-2 m-1 py-32">2</div>
+        <div class="grid_2 h-1/2 text-gray-700 text-center bg-gray-400 px-2 my-1 py-32">1</div>
+        <div class="grid_3 h-1/2 text-gray-700 text-center bg-gray-400 px-2 my-1 py-32">2</div>
       </div>
 
       <div class="w-1/3 flex flex-col bg-gray-200">
-        <div class="flex-1 text-gray-700 text-center bg-gray-400 px-4 m-1">1</div>
-        <div class="flex-1 text-gray-700 text-center bg-gray-400 px-4 m-1">2</div>
+        <div class="grid_4 flex-1 text-gray-700 text-center bg-gray-400 px-4 m-1">1</div>
+        <div class="grid_5 flex-1 text-gray-700 text-center bg-gray-400 px-4 mx-1">2</div>
         <div class="flex-1 text-gray-700 text-center bg-gray-400 px-4 m-1">3</div>
       </div>
     </div>
