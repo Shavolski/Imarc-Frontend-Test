3c4ee1e6dec58612e6c86cc24eed0d64214c285a
Shavolski-dev <stevewachiradev@gmail.com>
2020-11-12 02:08:42 -0500

    rearranged the icon on the trial button

--- src/components/section_1_navbar.js
+++ src/components/section_1_navbar.js
@@ -21,10 +21,7 @@ function Navbar() {
       {/* Part 3: The Trial button */}
       <div className="mx-12 px-8">
         <button className="trial_button flex flex-row text-white font-thin py-2 px-6 ml-48 mt-3">
-          FREE TRIAL
-          <svg className="flex-1 m-2" xmlns="http://www.w3.org/2000/svg" width="8.359" height="9.536" viewBox="0 0 8.359 9.536">
-            <path id="Path_490" data-name="Path 490" d="M494.73,230.4l-6.875-4.06a.763.763,0,0,0-1.144.675v7.975a.763.763,0,0,0,1.144.675l6.875-4.06a.7.7,0,0,0,0-1.2" transform="translate(-486.711 -226.231)" fill="#fff"/>
-          </svg>
+          FREE TRIAL &#9654;
         </button>
       </div>
     </div>
