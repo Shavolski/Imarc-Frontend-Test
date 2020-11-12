79951806489c0ed63eace595b46c06bf3e136587
Shavolski-dev <stevewachiradev@gmail.com>
2020-11-12 00:13:59 -0500

    social links and logo are added

--- src/components/section_6_footer.js
+++ src/components/section_6_footer.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import logo from '../assets/Logo.png';
+import social_icons from '../assets/social_icons.svg';
 
 //Footer section
 function Footer() {
   return (
     <div className="mt-12 mx-48">
-      <div className="main_footer h-40 bg-gray-400 flex">
-        <div class="w-2/6 p-2 bg-gray-400 text-center">.w-1/6</div>
+      <div className="main_footer h-32 bg-gray-400 flex">
+        <div class="w-2/6 bg-gray-400 text-center">
+          <img src={logo} />
+          <img src={social_icons} className="pt-3"/>
+        </div>
         <div class="w-1/6 p-2 bg-gray-500 text-center">.w-5/6</div>
         <div class="w-1/6 p-2 bg-gray-400 text-center">.w-1/6</div>
         <div class="w-1/6 p-2 bg-gray-500 text-center">.w-5/6</div>
