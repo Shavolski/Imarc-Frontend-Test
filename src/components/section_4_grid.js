6af23cd9f684385b859765d031b4ca7c45913dc1
Shavolski-dev <stevewachiradev@gmail.com>
2020-11-11 20:05:54 -0500

    grid is fully complete

--- src/components/section_4_grid.js
+++ src/components/section_4_grid.js
@@ -5,17 +5,17 @@ import ReactDOM from 'react-dom';
 function Grid() {
   return (
     <div class="flex bg-gray-200 mb-10 mx-48">
-      <div class="w-1/3 bg-gray-200">fff</div>
+      <div class="w-1/3 bg-gray-400 m-1 py-64">full space</div>
 
       <div class="w-1/3 bg-gray-200">
-        <div class="text-gray-700 text-center bg-gray-400 px-4 py-1 m-1">1</div>
-        <div class="text-gray-700 text-center bg-gray-400 px-4 py-1 m-1">2</div>
+        <div class="h-1/2 text-gray-700 text-center bg-gray-400 px-2 m-1 py-32">1</div>
+        <div class="h-1/2 text-gray-700 text-center bg-gray-400 px-2 m-1 py-32">2</div>
       </div>
 
-      <div class="w-1/3 bg-gray-200">
-        <div class="text-gray-700 text-center bg-gray-400 px-4 py-1 m-1">1</div>
-        <div class="text-gray-700 text-center bg-gray-400 px-4 py-1 m-1">2</div>
-        <div class="text-gray-700 text-center bg-gray-400 px-4 py-1 m-1">3</div>
+      <div class="w-1/3 flex flex-col bg-gray-200">
+        <div class="flex-1 text-gray-700 text-center bg-gray-400 px-4 m-1">1</div>
+        <div class="flex-1 text-gray-700 text-center bg-gray-400 px-4 m-1">2</div>
+        <div class="flex-1 text-gray-700 text-center bg-gray-400 px-4 m-1">3</div>
       </div>
     </div>
   );
