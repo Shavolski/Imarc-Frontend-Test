fed631cd726f6a68825c6d9d5b5271b32c1af406
Shavolski-dev <stevewachiradev@gmail.com>
2020-11-12 05:24:11 -0500

    landing page is responsive

--- src/components/section_2_landing_page.js
+++ src/components/section_2_landing_page.js
@@ -6,11 +6,11 @@ function Landing_page() {
   return (
     <div className="landing-page w-screen h-full -mt-6">
       {/* space to and from the picture */}
-      <div className="py-48 pl-48">
-        <h1 className="font-bold text-white text-6xl pb-20">LOREM IPSUM</h1>
+      <div className="py-10 lg:py-48 lg:pl-48">
+        <h1 className="font-bold text-white text-3xl lg:text-6xl lg:pb-20 pl-2 lg:pl-0">LOREM IPSUM</h1>
         {/* The smaller text */}
         <div className="flex flex-col">
-          <p className="text-lg">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <p className=" w-48 pl-2 lg:pl-0 lg:w-full text-sm lg:text-lg">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </div>
       </div>
     </div>
