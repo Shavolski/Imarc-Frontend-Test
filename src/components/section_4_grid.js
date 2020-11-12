3a07fc8619524c5150d01389979b96cf022fab2f
Shavolski-dev <stevewachiradev@gmail.com>
2020-11-11 20:56:31 -0500

    created bottom tabs to hold info on the grid

--- src/components/section_4_grid.js
+++ src/components/section_4_grid.js
@@ -4,18 +4,49 @@ import ReactDOM from 'react-dom';
 //Grid section
 function Grid() {
   return (
-    <div class="flex bg-gray-200 mb-10 mx-48">
-      <div class="grid_1 w-1/3 bg-gray-400 m-1 py-64">full space</div>
-
-      <div class="w-1/3 bg-gray-200">
-        <div class="grid_2 h-1/2 text-gray-700 text-center bg-gray-400 px-2 my-1 py-32">1</div>
-        <div class="grid_3 h-1/2 text-gray-700 text-center bg-gray-400 px-2 my-1 py-32">2</div>
+    <div className="flex mb-10 mx-48">
+      <div className="grid_1 w-1/3 py-64 relative m-1">
+        <div className="absolute bottom-0 right-0 h-20 w-full bg-gray-700">
+          <p>ff</p>
+          <p>ffr</p>
+        </div>
       </div>
 
-      <div class="w-1/3 flex flex-col bg-gray-200">
-        <div class="grid_4 flex-1 text-gray-700 text-center bg-gray-400 px-4 m-1">1</div>
-        <div class="grid_5 flex-1 text-gray-700 text-center bg-gray-400 px-4 mx-1">2</div>
-        <div class="flex-1 text-gray-700 text-center bg-gray-400 px-4 m-1">3</div>
+      <div className="w-1/3">
+        <div className="grid_2 h-1/2 text-gray-700 text-center px-2 py-32 my-1 relative">
+          <div className="absolute bottom-0 right-0 h-20 w-full bg-gray-700">
+            <p>ff</p>
+            <p>ffr</p>
+          </div>
+        </div>
+
+        <div className="grid_3 h-1/2 text-gray-700 text-center px-2 py-32 my-1 relative">
+          <div className="absolute bottom-0 right-0 h-20 w-full bg-gray-700">
+            <p>ff</p>
+            <p>ffr</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="w-1/3 flex flex-col">
+        <div className="grid_4 flex-1 text-gray-700 text-center px-4 m-1 relative">
+          <div className="absolute bottom-0 right-0 h-20 w-full bg-gray-700">
+            <p>ff</p>
+            <p>ffr</p>
+          </div>
+        </div>
+        <div className="grid_5 flex-1 text-gray-700 text-center px-4 mx-1 relative">
+          <div className="absolute bottom-0 right-0 h-20 w-full bg-gray-700">
+            <p>ff</p>
+            <p>ffr</p>
+          </div>
+        </div>
+        <div className="flex-1 text-gray-700 text-center bg-gray-400 px-3 m-1 relative">
+          <div className="absolute bottom-0 right-0 h-20 w-full bg-gray-700">
+            <p>ff</p>
+            <p>ffr</p>
+          </div>
+        </div>
       </div>
     </div>
   );
